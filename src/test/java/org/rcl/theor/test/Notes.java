137a24bccebda09dbbb4fd477f7dddcedbce8080
Moxious <moxious@oldhat.org>
2014-08-20 09:34:14 -0400

    basic test cases

---
+++ src/test/java/org/rcl/theor/test/Notes.java
@@ -0,0 +1,27 @@
+package org.rcl.theor.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.rcl.theor.Note;
+
+public class Notes {
+	@Test
+	public void testNotes() {
+		Note c = Note.MIDDLE_C;
+		
+		assertTrue("C's relative minor is A", c.getRelativeMinor().getToneClass() == Note.A);
+		assertTrue("C's MIDI number is 60", c.getMIDI() == 60);					
+		assertTrue("C's relative major is Eb", c.getRelativeMajor().getToneClass() == Note.EF);		
+		assertTrue("C's dominant is G", c.getDominant().getToneClass() == Note.G);
+		assertTrue("C's subdominant is F", c.getSubdominant().getToneClass() == Note.F);
+		
+		double freq = c.getFrequency();
+		// Due to double math, it's hard to ask for the value to be exact.
+		assertTrue("C's frequency is really close to reference value 261.626", freq >= 261.625 && freq <= 261.627);
+		
+		assertTrue("C is natural", c.isNatural());
+		assertTrue("C is not sharp", !c.isSharp());
+		
+	}
+}
---
+++ src/test/java/org/rcl/theor/test/Scales.java
@@ -0,0 +1,25 @@
+package org.rcl.theor.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.rcl.theor.NSequence;
+import org.rcl.theor.Note;
+import org.rcl.theor.scale.Scale;
+
+public class Scales {
+	@Test
+	public void testScales() {
+		NSequence ns = Scale.MAJOR.apply(Note.MIDDLE_C);
+		
+		Note[] expectedNotes = new Note[] {
+			new Note(Note.C), new Note(Note.D),
+			new Note(Note.E), new Note(Note.F),
+			new Note(Note.G), new Note(Note.A),
+			new Note(Note.B)
+		};
+		
+		for(int x=0; x<expectedNotes.length; x++) 
+			assertTrue("C major contains expected note " + expectedNotes[x], ns.contains(expectedNotes[x], false));				
+	}
+}
