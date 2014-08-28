2d6492c9abc1edad7a6be0fd600f466c904b781a
Moxious <moxious@oldhat.org>
2014-08-28 11:30:08 -0400

    first commit

---
+++ src/main/java/org/rcl/theor/interval/IntervalVector.java
@@ -0,0 +1,71 @@
+package org.rcl.theor.interval;
+
+import java.util.List;
+
+import org.rcl.theor.note.PitchClassSet;
+
+/**
+ * An interval vector is an array that expresses the intervallic content of a pitch-class set. 
+ * Some texts call these APIC vector (or absolute pitch-class interval vector).
+ * 
+ * @see http://en.wikipedia.org/wiki/Interval_vector
+ * @author moxious
+ */
+public class IntervalVector {
+	/** Number of times interval class 1 occurs (interval difference of 1 or 11) */
+	public int ic1 = 0;
+	/** Number of times interval class 2 occurs (interval difference of 2 or 10) */
+	public int ic2 = 0;
+	/** Number of times interval class 3 occurs (interval difference of 3 or 9) */
+	public int ic3 = 0;
+	/** Number of times interval class 4 occurs (interval difference of 4 or 8) */
+	public int ic4 = 0;
+	/** Number of times interval class 5 occurs (interval difference of 5 or 7) */
+	public int ic5 = 0; 
+	/** Number of times interval class 6 occurs (interval difference of 6) */
+	public int ic6 = 0;
+	
+	public IntervalVector(int ic1, int ic2, int ic3, int ic4, int ic5, int ic6) {
+		this.ic1 = ic1;
+		this.ic2 = ic2;
+		this.ic3 = ic3;
+		this.ic4 = ic4;
+		this.ic5 = ic5;
+		this.ic6 = ic6;
+	}
+	
+	public static IntervalVector fromPitchClassSet(PitchClassSet pcs) { 
+		return fromPitchClassSet(pcs.getNaturalOrder());
+	}
+	
+	public static IntervalVector fromPitchClassSet(List<Integer> pcs) { 
+		int [] ics = new int [] {0, 0, 0, 0, 0, 0};
+		
+		// Count the all-pairs interval content.
+		for(int x=0; x<pcs.size(); x++) { 
+			for(int y=(x+1); y<pcs.size(); y++) {				
+				// Calculate the interval between two items
+				int i = pcs.get(x) - pcs.get(y);
+				int intervalClass = new Interval(i).getIntervalClass();
+				
+				// System.out.println("Between " + pcs.get(x) + " and " + pcs.get(y) + " is " + i + " => " + intervalClass); 
+				
+				// Seen this class one more time....
+				// NOTE!  Interval class 1 is at index *0* in this array. So subtract one.
+				ics[intervalClass-1]++;
+			}
+		}
+		
+		return new IntervalVector(ics[0], ics[1], ics[2], ics[3], ics[4], ics[5]);
+	}
+	
+	public String toString() { 
+		return new String("[" + 
+	                      ic1 + " " + 
+	                      ic2 + " " + 
+	                      ic3 + " " + 
+	                      ic4 + " " + 
+	                      ic5 + " " + 
+	                      ic6 + "]");
+	}
+} // End IntervalVector
---
+++ src/main/java/org/rcl/theor/note/PitchClassSets.java
@@ -0,0 +1,278 @@
+package org.rcl.theor.note;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import org.rcl.theor.interval.Interval;
+import org.rcl.theor.interval.IntervalVector;
+
+/**
+ * A set of methods that implement pitch class set relationships and operations.  This is generally done abstractly over
+ * lists of Integers.  Other types exist which implement the PitchClassSet interface, which use these methods to do some of 
+ * their work.
+ * @author moxious
+ */
+public class PitchClassSets {
+	public static List<Integer> normalForm(List<Integer> pcs) {
+		List<Integer> normalOrder = new ArrayList<Integer>(pcs);		
+		System.out.println("BASE: " + toString(normalOrder));
+		
+		List<?>[] rotations = enumerateRotations(normalOrder);
+		for(List<?> l : rotations) { System.out.println("ENUMERATION: " + toString(l)); }
+		
+		List<Integer> normalForm = chooseNormalForm(rotations);
+		System.out.println("NORMAL " + toString(normalForm));
+
+		return normalForm;
+	}
+	
+	/**
+	 * This implementation takes the "rigorous method" identified by this:
+	 * http://composertools.com/Theory/PCSets/PCSets3.htm 
+	 * The outline of the approach is to start with the normal order of the PCS.  Then enumerate all rotations of that set.
+	 * Then determine which rotation has the minimum distance between first and last elements of the set.  
+	 * The PCS is then transposed so its first element is zero.
+	 * @param set
+	 * @return
+	 */
+	public static List<Integer> primeForm(List<Integer> pcs) { 
+		List<Integer> normalForm = normalForm(pcs);
+		
+		// This is how much we need to transpose up, in order to get the first item to zero.
+		int transpositionInterval = 12 - normalForm.get(0);
+		
+		List<Integer> primeForm = new ArrayList<Integer>(normalForm);		
+		transpose(primeForm, transpositionInterval);
+		
+		System.out.println("TRANSPOSED " + toString(primeForm));
+		return primeForm;
+	}
+	
+	/**
+	 * Transpose the given pitch class set by a given interval.  This function modifies its argument.
+	 * @param pcs
+	 * @param interval
+	 * @return a transposed set.
+	 */
+	public static List<Integer> transpose(List<Integer> pcs, Integer interval) {	
+		validate(pcs); 
+		
+		for(int x=0; x<pcs.size(); x++) 
+			pcs.set(x, Math.abs((pcs.get(x) + interval) % 12));
+		
+		return pcs;
+	}
+	
+	/**
+	 * Invert the provided PCS.  This function modifies its argument.
+	 * @param pcs the pcs.
+	 */
+	public static List<Integer> invert(List<Integer> pcs) { 
+		validate(pcs);
+		
+		for(int x=0; x<pcs.size(); x++) { 
+			pcs.set(x, 12 - pcs.get(x));
+		}
+		
+		return pcs;
+	}
+
+	protected static void validate(List<Integer> pcs) { 
+		if(pcs == null) throw new RuntimeException("Invalid pcs: null"); 
+		if(pcs.size() == 0) throw new RuntimeException("Invalid pcs: empty"); 
+	}
+	
+	public static void main(String [] args) throws Exception { 
+		NSequence ns = new NSequence();
+		ns.add(new Note(Note.C)); ns.add(new Note(Note.D));
+		ns.add(new Note(Note.E)); ns.add(new Note(Note.G));
+		ns.add(new Note(Note.AS)); 
+		
+		System.out.println("Prime form " + primeForm(ns.getNaturalOrder()));
+		
+		System.out.println("Starting NS again " + ns);
+		System.out.println(toString(ns.getNormalOrder()));
+		System.out.println("IV: " + IntervalVector.fromPitchClassSet(ns));
+	}
+	
+	/**
+	 * Determine which rotation has the minimum distance between first and last items.  If it's a tie, compare the first and second 
+	 * items.  If that's a tie, compare first and third, and so on until the tie is resolved.
+	 * @param enumerations
+	 * @return the rotation with minimum distance between first and last items.
+	 */	
+	@SuppressWarnings("unchecked")
+	protected static List<Integer> chooseNormalForm(List<?>[] enumerations) {
+		if(enumerations == null || enumerations.length == 0) throw new RuntimeException("Invalid enumerations, empty or null");
+		
+		// We'll start by comparing first and last items.
+		int x=0, y=enumerations[0].size()-1;
+							
+		while(true) {
+			// First item is the index of the best difference so far, second item
+			// is what that difference is.  Third item is number of times seen.
+			// Initialize with values so that get wiped out on first iteration.
+			int [] bestDifferenceSoFar = new int[] { -1, Integer.MAX_VALUE, 0 };
+			
+			for(int z=0; z<enumerations.length; z++) {
+				List<?> l = enumerations[z];
+				
+				// Get the difference
+				int diff = (Integer)l.get(y) - (Integer)l.get(x);
+				
+				if(diff < bestDifferenceSoFar[1]) {
+					// Update with best difference so far.
+					bestDifferenceSoFar[0] = z;    // it's at index z
+					bestDifferenceSoFar[1] = diff; // this is how much of a difference there is.
+					bestDifferenceSoFar[2] = 1;    // we've seen this difference once.
+				} else if(diff == bestDifferenceSoFar[1]) {
+					// At this level, there was a "tie" -- two PCs have the same.
+					// Increment number of times seen.  But we might still find a better difference,
+					// so we have to keep going.
+					bestDifferenceSoFar[2] = bestDifferenceSoFar[2] + 1;
+				}
+			}
+		
+			if(bestDifferenceSoFar[2] == 1) {
+				// We found a best difference between the x and the y'th elements.  There's only one
+				// PCS that has that, so we're done.
+				return (List<Integer>)enumerations[bestDifferenceSoFar[0]];
+			}
+			
+			// Otherwise, we need to keep checking, adjusting the y values.
+			// The algorithm says compare first and last items first.  Then look at first and second,
+			// first and third, and so on.
+			if(y == enumerations[0].size() - 1) 
+				y = 1;  // Look at first and second (x=0, y=1)
+			else if(y == enumerations[0].size() - 2) {
+				// Degenerate error case.  We've looked through all combinations.  There's *still* a tie?
+				// If we increment y now,
+				// we'll be checking a case we already checked.  So throw an exception here instead of getting
+				// stuck in an infinite loop.
+				if(bestDifferenceSoFar[0] >= 0) {
+					// This shouldn't be possible (a tie after this many rounds) this is just added for safety.
+					System.err.println("Warning, returning index " + bestDifferenceSoFar[0] + " despite continued tie after " + 
+				                       (y+1) + " iterations looking.");
+					return (List<Integer>)enumerations[bestDifferenceSoFar[0]];
+				} else
+					// This should be impossible.
+					throw new RuntimeException("Failed to find best PCS rotation after checking all possibilities");
+			} else { 
+				y++;   // Look at the first and the next yth.
+			}
+		}		
+	}
+	
+	/**
+	 * Takes a PCS, and returns a list of List<Integer>, corresponding to all rotations of the PCS.
+	 * The first is always the normal form.
+	 * @param set
+	 * @return a List[].   All lists are guaranteed to contain integers.
+	 */
+	protected static List<?>[] enumerateRotations(List<Integer> pcs) { 
+		List<Integer> normalOrder = new ArrayList<Integer>(pcs);
+		Collections.sort(normalOrder);
+		validate(normalOrder); 
+		
+		List<Integer> latestRotation = new ArrayList<Integer>();
+		
+		int y = normalOrder.size();
+		
+		// A set of y items has a total of y rotations.
+		List<?> [] rotations = new List[y] ;
+		
+		rotations[0] = normalOrder;
+		latestRotation = new ArrayList<Integer>(normalOrder);
+		
+		if(y == 1) return rotations;
+		
+		for(int x=1; x<y; x++) { 			
+			// Rotate it by 1.
+			// System.out.print("Rotating " + toString(latestRotation) + " => ");
+			Collections.rotate(latestRotation, -1);
+			// System.out.print(toString(latestRotation) + " => ");
+			// Add 12 to the last element.
+			int lastItem = latestRotation.size() - 1;
+			latestRotation.set(lastItem, latestRotation.get(lastItem) + 12);
+			
+			// System.out.println(toString(latestRotation));
+			
+			List<Integer> copy = new ArrayList<Integer>(latestRotation);			
+			rotations[x] = copy;
+		}
+
+		return rotations;
+	}
+	
+	/**
+	 * Determine whether an interval exists between two PCSs that make them equivalent.  This will always return null
+	 * if the sizes are different.
+	 * @param a a PCS
+	 * @param b a PCS
+	 * @return an interval, which when applied, will transform one PCS to the other, or null, if no such interval exists.
+	 */
+	public static Interval findEquivalentTranspositionInterval(List<Integer> a, List<Integer> b) { 
+		validate(a);
+		validate(b);
+
+		List<Integer> normalA = normalForm(a);
+		List<Integer> normalB = normalForm(b);
+		
+		// If
+		if(normalA.size() != normalB.size()) return null;
+		
+		// The interval between the first items.
+		int interval = Math.abs(normalA.get(0) - normalB.get(0));
+		
+		// Check if it works for the remaining items.
+		for(int x=1; x<normalA.size(); x++) { 
+			int thisInterval = Math.abs(normalA.get(x) - normalB.get(x));
+			
+			// Can't transpose these, because you need a different interval here.
+			if(thisInterval != interval) return null;
+		}
+		
+		// The originally found interval works everywhere, so it's good.
+		return new Interval(interval);
+	}
+	
+	/**
+	 * Determine whether the prime forms of two sets are equivalent or not.
+	 * @param a a pcs
+	 * @param b a pcs
+	 * @return true if the prime form of a and b are equal, false otherwise.
+	 */
+	public static boolean equivalent(List<Integer> a, List<Integer> b) {
+		validate(a);
+		validate(b);
+		
+		List<Integer> primeA = primeForm(a);
+		List<Integer> primeB = primeForm(b);
+		
+		if(primeA.size() != primeB.size()) return false;
+		
+		for(int x=0; x<primeA.size(); x++) { 
+			if(primeA.get(x) != primeB.get(x)) return false;
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * @param pcs a pitch class set
+	 * @return a String representation of the form e.g. [0, 1, 2, 4]
+	 */
+	public static String toString(List<?> pcs) { 
+		StringBuffer b = new StringBuffer("[");
+		
+		for(int x=0; x<pcs.size(); x++) { 
+			b.append(pcs.get(x));
+			
+			if(x < pcs.size() - 1) b.append(", "); 
+		}
+		
+		b.append("]");
+		return b.toString();
+	}
+}
